783b801291fa93a2897480f1c363709ec1671123
YYJ <yyj2301@gmail.com>
2023-09-24 15:50:47 +0900

    AWS 설정

--- lulu/src/App.vue
+++ lulu/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <IndexPage msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IndexPage from './components/IndexPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    IndexPage
   }
 }
 </script>
---
+++ lulu/src/components/IndexPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <div id="youtubeContents">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ybphvtxbRPs?si=R2gMdKC_xSMuJhnU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
